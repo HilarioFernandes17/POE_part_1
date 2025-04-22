d0e9aaa9f6931c49ae2b8b5eec2a3c75e54bc80e
st10497336@vcconnect.edu.za <st10497336@vcconnect.edu.za>
2025-04-22 19:55:31 +0200

    change

--- src/main/java/com/mycompany/poe_part_1/POE_part_1.java
+++ src/main/java/com/mycompany/poe_part_1/POE_part_1.java
@@ -44,7 +44,7 @@ public class POE_part_1 {
         System.out.println("Enter Username");
         String loginUsername = scanner.nextLine();
 
-        System.out.println("Enter Password");
+        System.out.println("Enter Passworddd");
         String loginPassword = scanner.nextLine();
 
         //Cheking Login
